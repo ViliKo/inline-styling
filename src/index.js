42085bfcf8af668cbc1b641ab7af5f61405e3703
ViliKo <59921526+ViliKo@users.noreply.github.com>
2021-10-20 15:53:14 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const customStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+customStyle.color = "blue";
+
+// good for updating styles
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
